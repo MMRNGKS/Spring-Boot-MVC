b5287867177c28b3a8cb15974b38508e189a3537
admin <hanzel.mamarungkas09@gmail.com>
2024-09-12 11:57:24 +0800

    2nd commit

--- src/main/java/com/example/practice/student/StudentRepository.java
+++ src/main/java/com/example/practice/student/StudentRepository.java
@@ -2,9 +2,11 @@ package com.example.practice.student;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
